1f4cc7f275954b0f2b0e452c1d380859dd96e0a8
Mert Can YILMAZ <mertcanylmaz@gmail.com>
2022-02-21 10:29:10 +0300

    First Commit

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -1,15 +1,255 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
+import 'package:flutter_codelab_1/classes.dart';
+import 'package:flutter_codelab_1/main.dart';
+import 'package:flutter_codelab_1/splash_page.dart';
 
 class MountsApp extends StatelessWidget {
-  const MountsApp({ Key? key }) : super(key: key);
+  const MountsApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Welcome to Mounts of the World!"),),
-      
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: Center(
+          child: Icon(
+            Icons.terrain,
+            color: mainColor,
+            size: 40,
+          ),
+        ),
+        actions: const [SizedBox(width: 40, height: 40)],
+        iconTheme: IconThemeData(color: mainColor),
+      ),
+      drawer: Drawer(
+        child: Container(
+          padding: EdgeInsets.all(30),
+          color: mainColor,
+          alignment: Alignment.bottomLeft,
+          child: Icon(
+            Icons.terrain,
+            color: Colors.white,
+            size: 80,
+          ),
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          AppHeader(),
+          AppSearch(),
+          Expanded(child: AppMountListView()),
+          AppCategoryList(),
+        ],
+      ),
+    );
+  }
+}
+
+class AppHeader extends StatelessWidget {
+  const AppHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 30, right: 30, top: 30),
+      child: Row(
+        children: [
+          ClipOval(
+            child: Image.network(
+              "https://avatars.githubusercontent.com/u/75225566?v=4",
+              width: 50,
+              height: 50,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Hello Mert Can",
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "Good morning",
+                style: TextStyle(color: mainColor, fontSize: 13),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class AppSearch extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(30),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Discover",
+            style: TextStyle(fontWeight: FontWeight.w900, fontSize: 25),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              Expanded(
+                  child: Container(
+                height: 50,
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(10)),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.search,
+                      color: Colors.grey,
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      "search",
+                      style: TextStyle(color: Colors.grey),
+                    )
+                  ],
+                ),
+              )),
+              Container(
+                height: 50,
+                width: 50,
+                margin: EdgeInsets.only(left: 10),
+                decoration: BoxDecoration(
+                    color: mainColor, borderRadius: BorderRadius.circular(10)),
+                child: Icon(
+                  Icons.tune,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class AppMountListView extends StatelessWidget {
+  const AppMountListView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 150,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: mountItems.length,
+        itemBuilder: (context, index) {
+          MountModel currentMount = mountItems[index];
+          return Container(
+            alignment: Alignment.bottomLeft,
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.all(10),
+            width: 150,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                image: DecorationImage(
+                    image: NetworkImage(currentMount.path), fit: BoxFit.cover)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  currentMount.name,
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  currentMount.location,
+                  style: TextStyle(color: Colors.white),
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class AppCategoryList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Category",
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "See More",
+                  style: TextStyle(
+                      color: mainColor,
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+          ),
+          Container(
+            height: 100,
+            margin: EdgeInsets.only(left: 10),
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: categories.length,
+              itemBuilder: (context, index) {
+                CategoryModel currentCategory = categories[index];
+                return Container(
+                  width: 100,
+                  margin: EdgeInsets.only(top: 10, right: 10),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                          color: Colors.grey.withOpacity(0.2), width: 2),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        currentCategory.icon,
+                        color: mainColor,
+                      ),
+                      Text(
+                        currentCategory.category,
+                        style: TextStyle(
+                            fontSize: 12, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ),
     );
   }
 }
