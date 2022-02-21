18fe6682d325986af9c2c986b9244da0d225dcc8
Mert Can YILMAZ <mertcanylmaz@gmail.com>
2022-02-21 18:13:52 +0300

    UI Changes

--- lib/classes.dart
+++ lib/classes.dart
@@ -65,3 +65,11 @@ final List<CategoryModel> categories = [
   CategoryModel(category: "Beach", icon: Icons.beach_access),
   CategoryModel(category: "Hiking", icon: Icons.directions_walk)
 ];
+
+class AppBottomBarItem {
+  IconData? icon;
+  bool isSelected;
+  String label;
+
+  AppBottomBarItem({this.icon, this.label = '', this.isSelected = false});
+}
--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_codelab_1/classes.dart';
-import 'package:flutter_codelab_1/main.dart';
 import 'package:flutter_codelab_1/splash_page.dart';
 
 class MountsApp extends StatelessWidget {
@@ -44,6 +43,7 @@ class MountsApp extends StatelessWidget {
           AppSearch(),
           Expanded(child: AppMountListView()),
           AppCategoryList(),
+          AppBottomBar()
         ],
       ),
     );
@@ -249,6 +249,78 @@ class AppCategoryList extends StatelessWidget {
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class AppBottomBar extends StatefulWidget {
+  @override
+  _AppBottomBarState createState() => _AppBottomBarState();
+}
+
+class _AppBottomBarState extends State<AppBottomBar> {
+  List<AppBottomBarItem> barItems = [
+    AppBottomBarItem(icon: Icons.home, label: "Home", isSelected: true),
+    AppBottomBarItem(icon: Icons.explore, label: "Explore", isSelected: false),
+    AppBottomBarItem(
+        icon: Icons.turned_in_not, label: "Tag", isSelected: false),
+    AppBottomBarItem(
+        icon: Icons.person_outline, label: "Profile", isSelected: false)
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(color: Colors.white, boxShadow: [
+        BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 10,
+            offset: Offset.zero)
+      ]),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: List.generate(barItems.length, (index) {
+          AppBottomBarItem currentBarItem = barItems[index];
+          Widget barItemWidget;
+
+          if (currentBarItem.isSelected) {
+            barItemWidget = Container(
+              padding: EdgeInsets.only(left: 15, top: 5, bottom: 5, right: 15),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20), color: mainColor),
+              child: Row(
+                children: [
+                  Icon(
+                    currentBarItem.icon,
+                    color: Colors.white,
+                  ),
+                  SizedBox(width: 5),
+                  Text(
+                    currentBarItem.label,
+                    style: TextStyle(color: Colors.white),
+                  )
+                ],
+              ),
+            );
+          } else {
+            barItemWidget = IconButton(
+              icon: Icon(
+                currentBarItem.icon,
+                color: Colors.grey,
+              ),
+              onPressed: () {
+                setState(() {
+                  for (var item in barItems) {
+                    item.isSelected = item == currentBarItem;
+                  }
+                });
+              },
+            );
+          }
+          return barItemWidget;
+        }),
       ),
     );
   }
